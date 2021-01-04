0d05470135c4ba83372623629f7d10795418f6ba
mo <thinkgo@aliyun.com>
2021-01-04 10:16:46 +0800

    fix ci config add freeport fix readme

---
+++ extnet/freeport.go
@@ -0,0 +1,50 @@
+// Open Source License (BSD 3-Clause)
+// ----------------------------------
+// Copyright (c) 2014, Patrick Hayes / HighWire Press
+// source: https://github.com/slimsag/freeport
+
+package extnet
+
+import "net"
+
+// GetFreePort asks the kernel for a free open port that is ready to use.
+func GetFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", ":0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
+}
+
+// MustGetFreePort calls GetFreePort and panics on error
+func MustGetFreePort() int {
+	port, err := GetFreePort()
+	if err != nil {
+		panic(err)
+	}
+	return port
+}
+
+// GetFreePorts asks the kernel for free open ports that are ready to use.
+func GetFreePorts(count int) ([]int, error) {
+	ports := make([]int, count)
+	for i := 0; i < count; i++ {
+		addr, err := net.ResolveTCPAddr("tcp", ":0")
+		if err != nil {
+			return nil, err
+		}
+		l, err := net.ListenTCP("tcp", addr)
+		if err != nil {
+			return nil, err
+		}
+		defer l.Close()
+		ports[i] = l.Addr().(*net.TCPAddr).Port
+	}
+	return ports, nil
+}
